b48971248c6f7225dd44fc214c2ce834c069f5d3
sm0key1 <devriesjabu@gmail.com>
2023-10-29 21:11:08 +0200

    IWA_07 Challenge Solutions

---
+++ IWA7/challenge2/script.js
@@ -0,0 +1,22 @@
+const nickname = "Timmy";
+const firstname = "Timothy";
+
+console.log("Good Morning", `${nickname}` && `${firstname}!`);
+
+// good morning
+
+// nickname && || firstname
+
+// if nickname is present
+
+// nickname OVER  firstname
+
+// if no nickname is present
+
+// firstname Over all . perhaps else
+
+/*************************************/
+
+// console.log expression Needs to be changed
+
+// if neither a firstname nor nickname is present only Good morning!
